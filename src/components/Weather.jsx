195812332201615be69e2a69125f5a1c37e42376
kavitha0802 <kavithaaudhayakumar2005gmail.com>
2025-08-08 07:51:52 +0530

    project updated

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -34,7 +34,7 @@ const Weather = () => {
           />
           <button
             onClick={fetchWeather}
-            className="bg-blue-500 text-white px-4 py-2 rounded"
+            className="bg-blue-600 text-white px-4 py-2 rounded"
           >
             Search
           </button>
